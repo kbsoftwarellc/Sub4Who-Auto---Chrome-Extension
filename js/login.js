2544d4a0bc506084bf9630ac457fa84929ede53c
tehaon <kenny@pwnlog.com>
2011-10-14 13:10:54 -0500

    adding localstorage

---
+++ js/login.js
@@ -0,0 +1,7 @@
+var username = localStorage["username"];
+var password = localStorage["password"];
+
+document.getElementById("username").value = username;
+document.getElementById("password").value = password;
+
+alert('done!');
